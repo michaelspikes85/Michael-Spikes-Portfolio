3c83366199df1668aee3a1d251002d371cf2ce57
Michael Spikes <michael.spikes85@gmail.com>
2020-08-29 15:08:02 -0400

    deployable version

---
+++ precache-manifest.4bb3198304e97e1182fc4281867541f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ad99f298e43f5131123bfe6bd1469b9",
+    "url": "/Michael-Spikes-Portfolio/index.html"
+  },
+  {
+    "revision": "dedfc3cd80c972163a7a",
+    "url": "/Michael-Spikes-Portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a63a99451f29e80250c4",
+    "url": "/Michael-Spikes-Portfolio/static/css/main.1201fed7.chunk.css"
+  },
+  {
+    "revision": "dedfc3cd80c972163a7a",
+    "url": "/Michael-Spikes-Portfolio/static/js/2.5ab2cabc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Michael-Spikes-Portfolio/static/js/2.5ab2cabc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a63a99451f29e80250c4",
+    "url": "/Michael-Spikes-Portfolio/static/js/main.cc71e748.chunk.js"
+  },
+  {
+    "revision": "cc3514b04be8cc2e7e17",
+    "url": "/Michael-Spikes-Portfolio/static/js/runtime-main.d4978616.js"
+  }
+]);
